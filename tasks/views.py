85fac9c235d747813f1c2eb711bdef07746c7d21
Kennedy Mutuku <kenmsimba@gmail.com>
2026-08-29 22:44:54 +0300

    added  views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,85 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, status, filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Task, TaskComment
+from .serializers import TaskSerializer, TaskCreateSerializer, TaskCommentSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for Task CRUD operations
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['status', 'priority', 'assigned_to']
+    search_fields = ['title', 'description']
+    ordering_fields = ['created_at', 'due_date', 'priority']
+    
+    def get_serializer_class(self):
+        if self.action in ['create', 'update', 'partial_update']:
+            return TaskCreateSerializer
+        return TaskSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+    
+    @action(detail=True, methods=['post'])
+    def add_comment(self, request, pk=None):
+        """
+        Add a comment to a task
+        """
+        task = self.get_object()
+        serializer = TaskCommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(task=task, user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    @action(detail=True, methods=['post'])
+    def change_status(self, request, pk=None):
+        """
+        Change task status
+        """
+        task = self.get_object()
+        new_status = request.data.get('status')
+        
+        if new_status not in dict(Task.STATUS_CHOICES):
+            return Response(
+                {'error': 'Invalid status'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        task.status = new_status
+        task.save()
+        serializer = self.get_serializer(task)
+        return Response(serializer.data)
+    
+    @action(detail=False, methods=['get'])
+    def my_tasks(self, request):
+        """
+        Get tasks created by the current user
+        """
+        tasks = Task.objects.filter(created_by=request.user)
+        serializer = self.get_serializer(tasks, many=True)
+        return Response(serializer.data)
+    
+    @action(detail=False, methods=['get'])
+    def assigned_to_me(self, request):
+        """
+        Get tasks assigned to the current user
+        """
+        tasks = Task.objects.filter(assigned_to=request.user)
+        serializer = self.get_serializer(tasks, many=True)
+        return Response(serializer.data)
+    
+    @action(detail=False, methods=['get'])
+    def overdue(self, request):
+        """
+        Get all overdue tasks
+        """
+        from django.utils import timezone
+        tasks = Task.objects.filter(due_date__lt=timezone.now()).exclude(status='DONE')
+        serializer = self.get_serializer(tasks, many=True)
+        return Response(serializer.data)
